987837d0b8e11f39b44db7e5e294bc09c6e8646d
demostudent10 <demostudent10@firstdraft.com>
2020-05-20 12:31:21 +0000

    Fix homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "application", :action => "homepage" })
+
   get("/movies/casablanca", { :controller => "movies", :action => "casablanca" })
   
   get("/movies/before2000", { :controller => "movies", :action => "before2k" })
