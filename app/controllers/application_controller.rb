afc965e5386e96e540507ce65a25d18803cb3abf
Raghu Betina <raghu@firstdraft.com>
2020-07-29 13:57:41 -0500

    Add routes and homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def homepage
-    render({ :template => "pages/home"})
+    render({ :template => "misc_templates/home.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,31 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
 
+  get("/directors", { :controller => "directors", :action => "list_directors" })
+  
+  get("/directors/youngest", { :controller => "directors", :action => "max_dob" })
+  
+  get("/directors/eldest", { :controller => "directors", :action => "min_dob" })
+
+  get("/movies", { :controller => "movies", :action => "all_movies" })
+
   get("/movies/:some_id", { :controller => "movies", :action => "details" })
   
-  get("/movies/before/:max_year", { :controller => "movies", :action => "oldies" })
-  
-  get("/movies/after/:min_year", { :controller => "movies", :action => "recent" })
-  
-  get("/directors/youngest", { :controller => "directors", :action => "last" })
-  
-  get("/directors/younger_than/:max_age", { :controller => "directors", :action => "youngins" })
-  
-  get("/directors/older_than/:min_age", { :controller => "directors", :action => "elders" })
-  
   get("/directors/:an_id", { :controller => "directors", :action => "bio" })
+  
+  get("/actors", { :controller => "actors", :action => "actor_directory" })
   
   get("/actors/:the_id", { :controller => "actors", :action => "profile" })
 
   get("/delete_movie/:toast_id", { :controller => "movies", :action => "destroy" })
 
   get("/insert_movie", { :controller => "movies", :action => "create" })
+
+  get("/delete_director/:bye_id", { :controller => "directors", :action => "sayonara" })
+
+  get("/add_director", { :controller => "directors", :action => "welcome" })
+
+  get("/delete_actor/:ciao_id", { :controller => "actors", :action => "toodles" })
+
+  get("/record_actor", { :controller => "actors", :action => "hiii" })
 end
